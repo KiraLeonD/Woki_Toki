47cf4d92d292aa5bab953feb685d74a4f7193153
marielbautista <marielalyssab@gmail.com>
2023-10-26 21:32:11 +0800

    SplashPageDarkmode

--- app/src/main/java/com/example/woki_toki/SplashActivity.java
+++ app/src/main/java/com/example/woki_toki/SplashActivity.java
@@ -1,19 +1,25 @@
 package com.example.woki_toki;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class SplashActivity extends AppCompatActivity {
-
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Hide the ActionBar/Toolbar
         getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
+        AppCompatDelegate();
 
         // Find the ConstraintLayout
         ConstraintLayout splashLayout = findViewById(R.id.splashLayout);
@@ -30,5 +36,13 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public void AppCompatDelegate(){
+        sp = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        boolean darkMODE = sp.getBoolean("dark", false);
+
+        if (darkMODE){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
     }
 }
